8a6e4380f39db5d1dcb2f1b15c86186156c00e40
Ramanops <kurmiraman0025@gmail.com>
2026-02-17 20:24:03 +0530

    backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './modules/auth/auth.module';
 import { JournalModule } from './modules/journal/journal.module';
 import { MoodModule } from './modules/mood/mood.module';
 import { StreakModule } from './modules/streak/streak.module';
 
 @Module({
-  imports: [AuthModule, JournalModule, MoodModule, StreakModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // loads .env
+    AuthModule,
+    JournalModule,
+    MoodModule,
+    StreakModule,
+  ],
 })
 export class AppModule {}
